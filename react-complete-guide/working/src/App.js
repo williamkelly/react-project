a4045fad04b4ddae7b348e6ccd90c924e0541079
William Kelly <william.kelly@blueyonder.co.uk>
2018-02-20 08:35:02 +0000

    some old work

--- react-complete-guide/working/src/App.js
+++ react-complete-guide/working/src/App.js
@@ -24,15 +24,42 @@ class App extends Component {
         ]
       });
   }
+
+  nameChangedHandler = (event) => {
+    this.setState( {
+      persons: [
+        {name: 'will', age: 41},
+        {name: event.target.value, age: 41},
+        {name: 'noah', age: 0}
+      ]
+    });
+}
   render() {
+    const style = {
+      'background-color': '#fff',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    };
+
     return (
       <div className="App">
           <h1>ello, i'm a react app</h1>
           <p>thiss is really working!</p>
-          <button onClick={this.switchNameHandler}>Switch Name</button>
-          <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-          <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>I'm noah mother</Person>
-          <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
+          <button 
+            style={style}
+            onClick={this.switchNameHandler}>Switch Name</button>
+          <Person 
+            name={this.state.persons[0].name} 
+            age={this.state.persons[0].age} />
+          <Person 
+            name={this.state.persons[1].name} 
+            age={this.state.persons[1].age}
+            change={this.nameChangedHandler}>I'm noah mother</Person>
+          <Person 
+            name={this.state.persons[2].name} 
+            age={this.state.persons[2].age} />
       </div>
     );
     //return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'ello, i\'m a react app'));
--- react-complete-guide/working/src/Person/Person.js
+++ react-complete-guide/working/src/Person/Person.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import './Person.css';
 const person = (props) => {
     return (
-        <div>
+        <div className="Person">
             <p>I'm name is {props.name} and i'm {props.age} years old.</p>
             <p>{props.children}</p>
+            <input type="text" onChange={props.change} value={props.name} />
         </div>
         //return <p>I'm a person! and i'm {Math.floor(Math.random() * 30)} years old.</p>
         )
