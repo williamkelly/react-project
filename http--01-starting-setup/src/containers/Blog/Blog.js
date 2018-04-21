4ec9a65dbeeb41c09a8a488d89e942cb7ee65a23
william.kelly <william.kelly@blueyonder.co.uk>
2018-04-21 15:03:13 +0100

    section 9 lecture 152 done.

--- http--01-starting-setup/src/containers/Blog/Blog.js
+++ http--01-starting-setup/src/containers/Blog/Blog.js
@@ -1,27 +1,54 @@
 import React, { Component } from 'react';
 
+import axios from 'axios';
 import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
 import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
 
 class Blog extends Component {
+
+  state = {
+    posts: [],
+  }
+
+  componentDidMount () {
+    axios.get('http://jsonplaceholder.typicode.com/posts')
+      .then(response => {
+        const posts = response.data.slice(0, 4);
+        const updatedPosts = posts.map(post => {
+          return {
+            ...post,
+            author: 'will',
+          }
+        })
+        this.setState({
+          posts: updatedPosts,
+        });
+        //console.log(response)
+      });
+  }
+
     render () {
+      const posts = this.state.posts.map(post => {
         return (
-            <div>
-                <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
-                </section>
-                <section>
-                    <FullPost />
-                </section>
-                <section>
-                    <NewPost />
-                </section>
-            </div>
+          <Post key={post.id} title={post.title} author={post.author} />
         );
+      });
+
+      return (
+          <div>
+              <section className="Posts">
+                  {posts}
+              </section>
+              <section>
+                  <FullPost />
+              </section>
+              <section>
+                  <NewPost />
+              </section>
+          </div>
+      );
     }
 }
 
