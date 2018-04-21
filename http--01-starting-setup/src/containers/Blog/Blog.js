16afaee53194007e90586ad46bb61c8b57a7c4cb
william.kelly <william.kelly@blueyonder.co.uk>
2018-04-21 15:35:46 +0100

    finished section 9 leacture 154:

--- http--01-starting-setup/src/containers/Blog/Blog.js
+++ http--01-starting-setup/src/containers/Blog/Blog.js
@@ -10,6 +10,7 @@ class Blog extends Component {
 
   state = {
     posts: [],
+    selectedPostId: null,
   }
 
   componentDidMount () {
@@ -29,27 +30,38 @@ class Blog extends Component {
       });
   }
 
-    render () {
-      const posts = this.state.posts.map(post => {
-        return (
-          <Post key={post.id} title={post.title} author={post.author} />
-        );
-      });
+  postSelectedHandler = (id) => {
+    this.setState({
+      selectedPostId: id,
+    });
+  }
 
+  render () {
+    const posts = this.state.posts.map(post => {
       return (
-          <div>
-              <section className="Posts">
-                  {posts}
-              </section>
-              <section>
-                  <FullPost />
-              </section>
-              <section>
-                  <NewPost />
-              </section>
-          </div>
+        <Post
+          key={post.id}
+          title={post.title}
+          author={post.author}
+          clicked={() => this.postSelectedHandler(post.id) }
+        />
       );
-    }
+    });
+
+    return (
+        <div>
+            <section className="Posts">
+                {posts}
+            </section>
+            <section>
+                <FullPost id={this.state.selectedPostId} />
+            </section>
+            <section>
+                <NewPost />
+            </section>
+        </div>
+    );
+  }
 }
 
 export default Blog;
