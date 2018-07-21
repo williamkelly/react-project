8e6ff0252bd7d90a0eb3c63f2b5109f02bb79b23
william kelly tv/pc <william.kelly@blueyonder.co.uk>
2018-07-21 17:38:41 +0100

    finished lecture section 12

---
+++ burger/src/containers/Orders/Orders.js
@@ -0,0 +1,46 @@
+import React, { Component, Fragment } from 'react';
+import Order from '../../components/Order/Order';
+import axios from '../../axios-orders';
+import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
+
+class Orders extends Component {
+    state = {
+        orders: [],
+        loading: true,
+    }
+
+    componentDidMount () {
+        axios.get('/orders.json')
+            .then(res => {
+                const fetchOrders = [];
+                for (let key in res.data) {
+                    fetchOrders.push({
+                        ...res.data[key],
+                        id: key,
+                    });
+                }
+                this.setState({ 
+                    loading: false,
+                    orders: fetchOrders,
+                });
+            })
+            .catch(err => {
+                this.setState({ loading: false });
+            });
+    }
+    render() {
+        return (
+            <Fragment>
+                {this.state.orders.map(order => (
+                    <Order 
+                        key={order.id}
+                        ingredients={order.ingredients}
+                        price={order.price}
+                    />
+                ))}
+            </Fragment>
+        );
+    }
+}
+
+export default withErrorHandler(Orders, axios);
